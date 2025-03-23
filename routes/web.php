bd4580326af05d6d70593df3128eeb2b424b6190
Muhammad Halim Dirgantara <halimdirgantara@gmail.com>
2025-03-23 11:30:46 +0700

    feat dashboard stat

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,19 @@
 
 use App\Livewire\Welcome;
 use App\Livewire\Auth\Login;
+use App\Livewire\Admin\Dashboard;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Welcome::class);
 
 Route::get('/login', Login::class)->name('login');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/logout', function () {
+        auth()->logout();
+        return redirect()->route('login');
+    })->name('logout');
+
+    Route::get('/', Welcome::class)->name('welcome');
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+});
